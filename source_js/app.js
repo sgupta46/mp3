87e02000b21caddbd4725e22120cb7b09e7bc408
Illuminati <Illuminati>
2016-10-17 23:00:03 -0500

    update

--- source_js/app.js
+++ source_js/app.js
@@ -1,5 +1,9 @@
 var app = angular.module('mp3',['ngRoute']);
 
 app.config(function ($routeProvider) {
-
-})
+  $routeProvider.
+    when('/gallery', { templateUrl: 'partials/gallery.html', controller: 'galleryController'}).
+    when('/details/:imdbID', { templateUrl: 'partials/details.html', controller: 'detailsController'}).
+    when('/list', { templateUrl: 'partials/list.html', controller: 'listController'}).
+    otherwise({redirectTo: '/list'});
+});
--- source_js/controllers.js
+++ source_js/controllers.js
@@ -1,1 +1,53 @@
 /* Sample Controller */
+
+app.controller('listController', ['$scope', '$http', function($scope, $http) {
+      $http.get('data/imdb250.json').success(function(data) {
+          $scope.movieDat = data;
+      }).error(function (error) {
+          console.log(error);
+      })
+
+      $scope.sortUpDown = "+";
+      $scope.sortBy = "title";
+      $scope.sortOrder = $scope.sortUpDown + $scope.sortBy;
+      $scope.sort = function() {
+        $scope.sortOrder = $scope.sortUpDown + $scope.sortBy;
+      }
+
+    }
+]);
+
+app.controller('galleryController', ['$scope', '$http', function($scope, $http) {
+    $http.get('data/imdb250.json').success(function(data) {
+          $scope.movieDat = data;
+          var sorted = $.unique($.map($scope.movieDat.map(function(d) {return d.genre}), function(c) {return c}));
+          $scope.genres = $.map(['All', sorted], function(c) {return c});
+    }).error(function (error) {
+        console.log(error);
+    });
+    $scope.filterGenre = '';
+    $scope.changeGenre = function(x) {
+            $scope.filterGenre = x === 'All' ? '' : x;
+    }
+}]);
+
+app.controller('detailsController', ['$scope', '$http', '$routeParams', '$filter', '$location', function($scope, $http, $routeParams, $filter, $location) {
+      $scope.imdbID = $routeParams.imdbID;
+      $http.get('data/imdb250.json').success(function(data) {
+              $scope.movieDat = data;
+              $scope.movie = $filter('filter')($scope.movieDat, { imdbID : $scope.imdbID});
+
+              var next, prev;
+              if($scope.movie[0].rank < 250) {
+                next = $scope.movie[0].rank + 1
+              }else next = 250;
+              if($scope.movie[0].rank > 1) {
+                prev = $scope.movie[0].rank -1
+              }else prev = 250;
+
+              $scope.next = $filter('filter')($scope.movieDat, { rank : next});
+              $scope.prev = $filter('filter')($scope.movieDat, { rank : prev});
+      }).error(function (error) {
+          console.log(error);
+      });
+}]);
